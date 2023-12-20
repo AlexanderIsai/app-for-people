edd5596d892fa634b037d084b4e32db7d524853e
AlexandrIsai <artat@ukr.net>
2023-12-20 21:10:00 +0200

    correction

--- src/main/java/de/telran/appforpeople/service/PersonServiceImpl.java
+++ src/main/java/de/telran/appforpeople/service/PersonServiceImpl.java
@@ -41,8 +41,7 @@ public class PersonServiceImpl implements PersonService {
                 }
             }
         }
-        int temp = characterStack.size();
-        for (int i = 0; i < temp; i++) {
+        while (!characterStack.empty()){
             stringBuilder.append(characterStack.pop());
         }
         return stringBuilder.toString();
